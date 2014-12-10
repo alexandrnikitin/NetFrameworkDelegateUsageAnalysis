778892b42c321bfb6bacc6794c1067bcf176fd98
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2014-12-10 19:43:52 +0200

    Straightforward implementation of IsCast check

--- Analysis/Program.cs
+++ Analysis/Program.cs
@@ -63,7 +63,8 @@ namespace Analysis
                                     /*IsEmpty(methodPointer) || */
                                     /*IsIdentity(methodPointer) ||*/
                                     /*IsAlwaysTrue(methodPointer) || */
-                                    IsCompatible(methodPointer)
+                                    /*IsCompatible(methodPointer) || */
+                                    IsCast(methodPointer)
                                     )
                                 {
                                     i++;
@@ -136,6 +137,24 @@ namespace Analysis
                 methodDefinition.Body.Instructions[3].OpCode != OpCodes.Ldnull ||
                 methodDefinition.Body.Instructions[4].OpCode != OpCodes.Cgt_Un ||
                 methodDefinition.Body.Instructions[5].OpCode != OpCodes.Ret)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsCast(MethodDefinition methodDefinition)
+        {
+            if (!methodDefinition.HasBody)
+            {
+                return false;
+            }
+
+            if (methodDefinition.Body.Instructions[0].OpCode != OpCodes.Ldarg_0 ||
+                methodDefinition.Body.Instructions[1].OpCode != OpCodes.Box ||
+                methodDefinition.Body.Instructions[2].OpCode != OpCodes.Unbox_Any ||
+                methodDefinition.Body.Instructions[3].OpCode != OpCodes.Ret)
             {
                 return false;
             }
